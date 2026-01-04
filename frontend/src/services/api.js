08a7b42c258ac955f98ccddc7032dc33f1580503
İbrahim Yılgör <yilgor16@itu.edu.tr>
2026-01-04 13:31:10 +0300

    feat: nginxtry1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,26 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
+
+const baseURL =
+	process.env.NODE_ENV === "production"
+		? "/api"
+		: window.location.protocol + "//" + window.location.hostname + ":5000";
 
 const api = axios.create({
-  baseURL: window.location.protocol + '//' + window.location.hostname + ':5000',
+	baseURL: baseURL,
 });
 
 // Add token
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = 'Bearer ' + token;
-  return config;
+api.interceptors.request.use((config) => {
+	const token = localStorage.getItem("token");
+	if (token) config.headers.Authorization = "Bearer " + token;
+	return config;
 });
 
 // Logout if token is invalid
 api.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/session-expired';
-    }
-    return Promise.reject(error);
-  }
+	(response) => response,
+	(error) => {
+		if (error.response && error.response.status === 401) {
+			localStorage.removeItem("token");
+			window.location.href = "/session-expired";
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default api;
