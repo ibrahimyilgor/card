73e02c79bcb7534604f0696aacd5f6e7719c8ebc
İbrahim Yılgör <yilgor16@itu.edu.tr>
2026-01-10 20:25:57 +0300

    WIP on removesv

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-	window.location.protocol + "//" + window.location.hostname + "/auth";
+const hostname = window.location.host.split(":")[0];
+const baseURL = window.location.protocol + "//" + hostname + "/auth";
 
 const api = axios.create({
 	baseURL: baseURL,
